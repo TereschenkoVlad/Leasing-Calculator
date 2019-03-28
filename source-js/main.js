3cd6318a5e4d42dddb960fb705fa13b51cb0c588
TereschenkoVlad <vlad.tereshchenko1999@gmail.com>
2019-03-28 21:34:49 +0200

    add speadsheet with a result for the every month

--- source-js/main.js
+++ source-js/main.js
@@ -5,26 +5,78 @@ button.addEventListener('click', function () {
     var startPrice = document.getElementById('how-much-cost').value;
     var termLeasing = document.getElementById('term-leasing').value;
     var tax = document.getElementById('leasing-procent').value;
-    var extraPrice = document.getElementById('last-price').value;
-    var showResult = document.querySelector('.result')
+    var firstPrice = document.getElementById('first-price').value;
+    // var showResult = document.querySelector('.result')
+    var armMonth = document.querySelector('#arm-month')
+    var onceTax = document.querySelector('#once-tax').value
+    console.log(onceTax);
     var result = 0;
-    function leasingCalculate(prise, lastPrice, term, tax) {
-        result = (prise - lastPrice * (1/(Math.pow(1 + tax, term)))) * (tax/(1 -(Math.pow(1 + tax, -term))));
-        return parseFloat(result.toFixed(2));
+
+    function leasingCalculate(startPrise, termLeasing, tax, firstPrice, armMonth, onceTax) {
+
+        var taxMounthC = tax / 12;
+        var oneTax = (startPrise / 100 * onceTax);
+        var firstPay = startPrise / 100 * firstPrice;
+        var payMonth = (startPrise - firstPay) / termLeasing
+        var needToPay = startPrise
+        let table = document.createElement('TABLE')
+        for (let i=0; i<termLeasing; i++) {
+            function payment (startPrise, firstPay, oneTax, taxMounthC, payMonth) {
+                var paymentMonth = 0;
+                if (i === 0) {
+                    paymentMonth = firstPay + oneTax + (needToPay / 100 * taxMounthC) + payMonth
+                    needToPay = startPrise - paymentMonth;
+                    return paymentMonth;
+                } else  {
+                    paymentMonth = payMonth + (needToPay / 100 * taxMounthC)
+                    needToPay -= paymentMonth
+                    return paymentMonth
+                }
+            }
+            // console.log(payment(startPrise, firstPay, oneTax, taxMounthC, payMonth).toFixed(2));
+
+            if (i) {
+                oneTax = 0
+            }
+
+            let monthToPay = {
+                numberOfMonth: i + 1,
+                laseObj: (+needToPay).toFixed(2),
+                paymentMonth: (+payMonth).toFixed(2),
+                taxClient: (+needToPay / 100 * taxMounthC).toFixed(2),
+                taxOne: oneTax,
+                finalMonthPay: payment(startPrise, firstPay, oneTax, taxMounthC, payMonth).toFixed(2)
+            }
+
+            let tr = document.createElement('TR')
+            table.appendChild(tr)
+            for (j=0; j<6; j++) {
+                let td = document.createElement('TD')
+                td.innerHTML = monthToPay[Object.keys(monthToPay)[j]]
+                tr.appendChild(td)
+            }
+
+            console.log(monthToPay);
+        }
+
+        resultBlock.appendChild(table)
     }
-    showResult.innerText = leasingCalculate(+startPrice, +extraPrice , +termLeasing, +tax / 12 / 100);
+
+    leasingCalculate(startPrice, termLeasing, tax, firstPrice, armMonth, onceTax)
     resultBlock.classList.add('active')
 
-});
+
+})
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-})
+});
 
-var inputsContainer = document.querySelector('.inputs-container')
+var inputsContainer = document.querySelector('.inputs-container');
 
 inputsContainer.addEventListener('click', function () {
-    resultBlock.classList.remove('active')
+    resultBlock.classList.remove('active');
     console.log(2222)
-})
+});
 
